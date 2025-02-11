68b6bd41e98fe2675258c60d685380250d5ba35f
renzabc <renzagamanos3@gmail.com>
2025-02-10 21:40:31 -0600

    created a maven spring boot project

---
+++ dnsmanager/src/main/java/com/dnsmanager/dnsmanager/DnsmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.dnsmanager.dnsmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnsmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DnsmanagerApplication.class, args);
+	}
+
+}
